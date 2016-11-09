4dd884b9949bd80fb6def614be79b32474b9ed8e
Jose Maximino Chavez <josemc.ortiz@gmail.com>
2016-11-08 23:12:23 -0400

    mas

---
+++ BLL/Utility.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+
+namespace BLL
+{
+    public static class Utility
+    {
+
+        public static void MensajeToastr(Page page, string message, string title, string type = "info")
+        {
+            page.ClientScript.RegisterStartupScript(page.GetType(), "toastr_message",
+                  String.Format("toastr.{0}('{1}', '{2}');", type.ToLower(), message, title), addScriptTags: true);
+        }
+   
+        public static int ConvierteEntero(string s)
+        {
+            int id = 0;
+            int.TryParse(s, out id);
+            return id;
+        }
+        public static float ConvierteFloat(string s)
+        {
+            float id = 0;
+            float.TryParse(s, out id);
+            return id;
+        }
+        // Estos metodos reciven un evento cuando se presiona una tecla en el textbox para Validarlos
+    }
+}
